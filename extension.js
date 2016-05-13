3119ff06384f283fa800c8b922ae7f4bf8f77372
Vlad Stirbu <vstirbu@gmail.com>
2016-05-13 16:44:02 +0300

    basic working version

--- extension.js
+++ extension.js
@@ -1,33 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-
-class MermaidProvider {
-    constructor () {
-        this._onDidChange =  new vscode.EventEmitter();
-    }
-    
-    provideTextDocumentContent (uri, token) {
-        console.log(uri, token);
-        return 'preview';
-    }
-    
-    get onDidChange () {
-        return this._onDidChange.event;
-    }
-    
-    update (uri) {
-        this._onDidChange.fire(uri);
-    }
-}
+const vscode = require('vscode');
+const mermaid = require('mermaid');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    class MermaidDocumentContentProvider {
+        constructor () {
+            this._onDidChange =  new vscode.EventEmitter();
+            this.graph = '';
+        }
+        
+        provideTextDocumentContent (uri, token) {
+            return `<!DOCTYPE html>
+            <html>
+            <head>
+            <link href="${context.extensionPath}/node_modules/mermaid/dist/mermaid.forest.css" rel="stylesheet">
+            <script src="${context.extensionPath}/node_modules/mermaid/dist/mermaid.min.js">
+            <script type="text/javascript">
+            mermaid.initialize({startOnLoad:true});
+            </script>
+            </head>
+            <body>
+            ${this.graph}
+            </body>`;
+        }
+        
+        get onDidChange () {
+            return this._onDidChange.event;
+        }
+        
+        update (uri) {
+            let editor = vscode.window.activeTextEditor;
+            let text = editor.document.getText();
+            let selStart = editor.document.offsetAt(editor.selection.anchor);
+            let propStart = text.lastIndexOf('<div', selStart);
+            let propEnd = text.indexOf('</div>', selStart);
+            
+            if (propStart === -1 || propEnd === -1) {
+                console.log("Cannot determine the rule's properties.");
+            }
+            
+            let graph = text.slice(propStart, propEnd + 6);
+            this.graph = graph;
+            this._onDidChange.fire(uri);
+        }
+    }
+    
     const registerCommand = vscode.commands.registerCommand;
     let previewUri = vscode.Uri.parse('mermaid-preview://authority/mermaid-preview');
     
-    let provider = new MermaidProvider();
+    let provider = new MermaidDocumentContentProvider();
     let registration = vscode.workspace.registerTextDocumentContentProvider('mermaid-preview', provider);
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -44,17 +68,20 @@ function activate(context) {
         });
     });
     
+    vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two).then((success) => {
+        }, (reason) => {
+            vscode.window.showErrorMessage(reason);
+        });
+    
     vscode.workspace.onDidChangeTextDocument((e) => {
         if (e.document === vscode.window.activeTextEditor.document) {
             provider.update(previewUri);
-            //vscode.window.showInformationMessage('changed');
         }
     });
 
     vscode.window.onDidChangeTextEditorSelection((e) => {
         if (e.textEditor === vscode.window.activeTextEditor) {
-            //provider.update(previewUri);
-            //vscode.window.showInformationMessage('selected');
+            provider.update(previewUri);
         }
     });
     
