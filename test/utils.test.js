95b1f61b966dff74a453a5008ed66165d53dc2eb
Vlad Stirbu <vstirbu@gmail.com>
2018-05-23 19:36:21 +0300

    added support for hugo shorcodes (fixed #48)

--- test/utils.test.js
+++ test/utils.test.js
@@ -8,35 +8,55 @@ const usesFontawesome = require('../lib/uses-fontawesome');
 suite('Utilities Tests', () => {
   const startFenced = '```mermaid';
   const startDived = '<div class="mermaid">';
+  const startHugo = '{{<mermaid align="left">}}';
 
   const getAbsoluteFilePath = filename => path.join(__dirname, 'fixtures', filename);
 
   const openTextDocument = filename => vscode.workspace.openTextDocument(vscode.Uri.file(getAbsoluteFilePath(filename)));
 
   suite('findDiagram', () => {
-    test('Detects fenced diagram if cursor inside fence', () =>
-      openTextDocument('sequence.md').then(document => {
-        assert.ok(findDiagram(document.getText(), startFenced.length));
-      })
-    );
+    suite('markdown', () => {
+      test('Detects fenced diagram if cursor inside fence', () =>
+        openTextDocument('sequence.md').then(document => {
+          assert.ok(findDiagram(document.getText(), startFenced.length));
+        })
+      );
+  
+      test('Does not detect fenced diagram if cursor outside fence', () =>
+        openTextDocument('sequence.md').then(document => {
+          assert.equal(findDiagram(document.getText(), 0), undefined);
+        })
+      );
+    });
 
-    test('Does not detect fenced diagram if cursor outside fence', () =>
-      openTextDocument('sequence.md').then(document => {
-        assert.equal(findDiagram(document.getText(), 0), undefined);
-      })
-    );
+    suite('html', () => {
+      test('Detects <div> diagram if cursor inside tag', () =>
+        openTextDocument('sequence.html').then(document => {
+          assert.ok(findDiagram(document.getText(), startDived.length));
+        })
+      );
+  
+      test('Does not detect <div> diagram if cursor outside tag', () => 
+        openTextDocument('sequence.html').then(document => {
+          assert.equal(findDiagram(document.getText(), 0), undefined);
+        })
+      );
+    });
 
-    test('Detects <div> diagram if cursor inside tag', () =>
-      openTextDocument('sequence.html').then(document => {
-        assert.ok(findDiagram(document.getText(), startDived.length));
-      })
-    );
+    suite('hugo', () => {
+      test('Detects hugo diagram if cursor inside tag', () =>
+        openTextDocument('hugo.html').then(document => {
+          assert.ok(findDiagram(document.getText(), startHugo.length));
+        })
+      );
+  
+      test('Does not detect hugo diagram if cursor outside tag', () => 
+        openTextDocument('hugo.html').then(document => {
+          assert.equal(findDiagram(document.getText(), 0), undefined);
+        })
+      );
+    });
 
-    test('Does not detect <div> diagram if cursor outside tag', () => 
-      openTextDocument('sequence.html').then(document => {
-        assert.equal(findDiagram(document.getText(), 0), undefined);
-      })
-    );
   });
 
   suite('usesFontawesome', () => {
