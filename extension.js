b9642b8ff12d6b7bdb0ddd9049d9fa9f3fe84c2a
Vlad Stirbu <vstirbu@gmail.com>
2018-05-24 15:02:11 +0300

    transitioned to webview

--- extension.js
+++ extension.js
@@ -1,60 +1,105 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-
-const MermaidDocumentContentProvider = require("./content-provider");
+const vscode = require('vscode');
+const findDiagram = require('./lib/find-diagram');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
   const registerCommand = vscode.commands.registerCommand;
-  let previewUri = vscode.Uri.parse(
-    "mermaid-preview://authority/mermaid-preview"
-  );
 
-  let provider = new MermaidDocumentContentProvider(context);
-  let registration = vscode.workspace.registerTextDocumentContentProvider(
-    "mermaid-preview",
-    provider
-  );
+  let command = registerCommand('mermaidPreview.start', () => {
+    const panel = vscode.window.createWebviewPanel(
+      'mermaidPreview',
+      'Mermaid Preview',
+      vscode.ViewColumn.Two,
+      {
+        enableScripts: true
+      }
+    );
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // console.log('Congratulations, your extension "mermaid-preview" is now active!');
+    const getContent = () => {
+      const mermaidPath = 'node_modules/mermaid/dist/mermaid.min.js';
+      const mermaidUrl = vscode.Uri.file(context.asAbsolutePath(mermaidPath)).with({
+        scheme: 'vscode-resource'
+      });
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = registerCommand("extension.previewMermaidDiagram", () => {
-    return vscode.commands
-      .executeCommand(
-        "vscode.previewHtml",
-        previewUri,
-        vscode.ViewColumn.Two,
-        "Mermaid Preview"
-      )
-      .then(
-        success => {},
-        reason => {
-          vscode.window.showErrorMessage(reason);
-        }
-      );
-  });
+      const fsStylesheetPath = 'node_modules/font-awesome/css/font-awesome.min.css';
+      const faBase = vscode.Uri.file(context.asAbsolutePath(fsStylesheetPath)).with({
+        scheme: 'vscode-resource'
+      });
+      
+      return `<!DOCTYPE html>
+<html>
+  <head>
+    <base href="">
+    <link rel="stylesheet" href="${faBase}">
+    <script src="${mermaidUrl}"></script>
+  </head>
+  <body>
+    <div id="diagram" class="mermaid" style="height: 100vh"></div>
 
-  vscode.workspace.onDidChangeTextDocument(e => {
-    if (e.document === vscode.window.activeTextEditor.document) {
-      provider.update(previewUri);
+    <div id="minimap" class="mermaid" style="position: fixed; top: 0; left: 0; width: 75px; height: 50px; z-index: 100; display: block"></div>
+    <script>
+      const minimap = document.getElementById('minimap');
+      const diagram = document.getElementById('diagram');
+
+      window.addEventListener('message', event => {
+        const message = event.data;
+
+        minimap.textContent = message.diagram;
+        minimap.removeAttribute('data-processed');
+        
+        diagram.textContent = message.diagram;
+        diagram.removeAttribute('data-processed');
+
+        mermaid.init();
+      });
+
+      const config = {
+        startOnLoad: false,
+        theme: 'forest'
+      };
+      mermaid.initialize(config);
+    </script>
+  </body>
+</html>`;
+    };
+
+    const previewHandler = () => {
+      const editor = vscode.window.activeTextEditor;
+      const text = editor.document.getText();
+      const cursor = editor.document.offsetAt(editor.selection.anchor);
+      
+      const diagram = findDiagram(text, cursor);
+      
+      panel.webview.postMessage({
+        diagram
+      });
     }
+    
+    vscode.workspace.onDidChangeTextDocument(e => {
+      if (e.document === vscode.window.activeTextEditor.document) {
+        previewHandler();
+      }
+    });
+    
+    vscode.window.onDidChangeTextEditorSelection(e => {
+      if (e.textEditor === vscode.window.activeTextEditor) {
+        previewHandler();
+      }
+    });
+
+    panel.onDidDispose(() => {
+      console.log('panel closed');
+      clearInterval(interval);
+    }, null, context.subscriptions);
+  
+    panel.webview.html = getContent();
   });
+  
 
-  vscode.window.onDidChangeTextEditorSelection(e => {
-    if (e.textEditor === vscode.window.activeTextEditor) {
-      provider.update(previewUri);
-    }
-  });
-
-  context.subscriptions.push(disposable, registration);
-
+  context.subscriptions.push(command);
 
   return {
     extendMarkdownIt(md) {
