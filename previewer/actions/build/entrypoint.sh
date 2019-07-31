9e53bfc25155e6e2870257fab9d5a31576ee69d3
Vlad Știrbu <vstirbu@gmail.com>
2019-07-31 14:57:49 +0300

    added preview app to publishing pipeline. fixed #85

---
+++ previewer/actions/build/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+cd previewer
+
+# Install dependencies and build app
+npm install
+npm run build
+
+# Copy font-awesome files
+mkdir -p dist/vendor/font-awesome/css
+cp node_modules/font-awesome/css/font-awesome.min.css dist/vendor/font-awesome/css/
+cp node_modules/font-awesome/css/font-awesome.css.map dist/vendor/font-awesome/css/
+cp -r node_modules/font-awesome/fonts dist/vendor/font-awesome/
